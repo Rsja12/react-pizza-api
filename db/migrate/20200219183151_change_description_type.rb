99b4b79e98491c0159c7e27fb3202ea2e4d7982d
Rsja12 <asocarras10@gmail.com>
2020-02-19 13:32:20 -0500

    adds migration file for changing descritption datatype

---
+++ db/migrate/20200219183151_change_description_type.rb
@@ -0,0 +1,4 @@
+class ChangeDescriptionType < ActiveRecord::Migration[6.0]
+  def change
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Pizza.create(name: 'Margherita', description: 'tomato sauce, mozzarela, basil')
