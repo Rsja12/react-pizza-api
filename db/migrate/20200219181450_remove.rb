763578c297da93f41e15347d07f5c9eb68c35e23
Rsja12 <asocarras10@gmail.com>
2020-02-19 13:16:43 -0500

    removes price column from pizza table

--- db/migrate/20200219181450_remove.rb
+++ db/migrate/20200219181450_remove.rb
@@ -1,4 +1,5 @@
 class Remove < ActiveRecord::Migration[6.0]
   def change
+    remove_column :pizzas, :price
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_19_175031) do
+ActiveRecord::Schema.define(version: 2020_02_19_181450) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,7 +18,6 @@ ActiveRecord::Schema.define(version: 2020_02_19_175031) do
   create_table "pizzas", force: :cascade do |t|
     t.string "flavor"
     t.string "description"
-    t.money "price", scale: 2
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
