51bca658b90ea23a53fb4373ad5d475e0a67d4c2
Rsja12 <asocarras10@gmail.com>
2020-03-21 16:09:41 -0400

    adds private methods

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -15,4 +15,14 @@ class CartsController < ApplicationController
   def destroy
   end
 
+  private 
+
+  def set_cart
+    @cart = Cart.find_by(id: params[:id])    
+  end
+
+  def cart_params
+    params.require(:cart).permit(:name, :description, :subtotal, :tax, :total)
+  end
+
 end
